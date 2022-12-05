4a28ea763b069862548d276382a3bb15c9dc574f
hannaforssell <forssellhanna@gmail.com>
2022-12-05 12:42:36 +0100

    🎉 Starts project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,5 +32,6 @@ module.exports = {
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
   }
 }
